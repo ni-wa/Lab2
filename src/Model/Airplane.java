9637380c4158ef9c921328be511857dc855b5844
larslarslindholm <lindholm.edu@gmail.com>
2016-07-11 17:06:24 +0200

    Flight: Constructor upd, Airplane: getNumberOfPhysicalSeats added

--- src/Model/Airplane.java
+++ src/Model/Airplane.java
@@ -4,7 +4,14 @@ import java.util.List;
 
 public class Airplane {
 
-    private List<PhysicalSeat> physicalSeat;
+    private List<PhysicalSeat> physicalSeats;
 
     private String status;
+    
+    List<PhysicalSeat> getPhysicalSeats(){
+        return physicalSeats;
+    }
+    int getNumberOfPhysicalSeats(){
+        return physicalSeats.size();
+    }
 }
--- src/Model/Flight.java
+++ src/Model/Flight.java
@@ -7,21 +7,26 @@ import java.util.List;
 
 public class Flight {
 
+    private Airplane airplane;
     private ArrayList<Seat> seats;
     private LocalDateTime departure;
-    private Airplane airplane;
+    private LocalDateTime arrival;
     private int firstClassPrice;
     private int secondClassPrice;
     private Menu menu;
-    private LocalDateTime arrival;
       
-    public Flight(Menu menu, Airplane airplane){
+    public Flight(Airplane airplane, Menu menu){
+        this.airplane = airplane;
+        //When a flight is defined, a list of bookable Seats is defined 
+        //with equal number of seats as available Physical seats 
+        //in the actual type of Airplane.
+        Seat[] seats = new Seat[airplane.getNumberOfPhysicalSeats()];
+
+        departure = LocalDateTime.of(2016,12,24,12,00);
+        arrival = LocalDateTime.of(2016,12,24,13,00);
         firstClassPrice = 2222;
         secondClassPrice = 555;
         this.menu = menu;
-        this.airplane = airplane;
-        departure = LocalDateTime.of(2016,12,24,12,00);
-        arrival = LocalDateTime.of(2016,12,24,13,00);
     }
     
     public Seat createBooking(Customer customer, Food food, Seat seat) {
@@ -38,5 +43,9 @@ public class Flight {
 
     private List<PhysicalSeat> convertPhysicalSeatsToSeats(List<PhysicalSeat> physicalSeats) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public String showBookingInformationForSeat(Seat seat){
+        throw new UnsupportedOperationException("Not supported yet.");       
     }
 }
