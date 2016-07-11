714d9afbe2ef9caecaf8367c8ea6a68f2ee9d396
Niclas Wahlgren <niclaswmail@gmail.com>
2016-07-11 16:43:23 +0200

    new constructor added

--- src/Model/Airplane.java
+++ src/Model/Airplane.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class Airplane {
 
+    public Airplane(){
+    }
+
+    public Airplane(List<PhysicalSeat> physicalSeats, String status) {
+        this.physicalSeat = physicalSeats;
+        this.status = status;
+    }
+    
     private List<PhysicalSeat> physicalSeat;
 
     private String status;
