02316c3b984b2a49b9bb25f594bc1737255813fb
larslarslindholm <lindholm.edu@gmail.com>
2016-07-11 15:45:45 +0200

    simple constructor upd

--- src/Model/Flight.java
+++ src/Model/Flight.java
@@ -1,30 +1,29 @@
 package Model;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Flight {
 
     private ArrayList<Seat> seats;
-
     private LocalDateTime departure;
-
     private Airplane airplane;
-
     private int firstClassPrice;
-
     private int secondClassPrice;
-
     private Menu menu;
-
     private LocalDateTime arrival;
-    
-    public Flight(){
-        this.firstClassPrice = 2222;
-        this.secondClassPrice = 555;
+      
+    public Flight(Menu menu, Airplane airplane){
+        firstClassPrice = 2222;
+        secondClassPrice = 555;
+        this.menu = menu;
+        this.airplane = airplane;
+        departure = LocalDateTime.of(2016,12,24,12,00);
+        arrival = LocalDateTime.of(2016,12,24,13,00);
     }
-
+    
     public Seat createBooking(Customer customer, Food food, Seat seat) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
