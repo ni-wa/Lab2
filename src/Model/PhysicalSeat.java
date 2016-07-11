e0ca1d44040cd2f32da17b8c1edb322810ccc9bd
Niclas Wahlgren <niclaswmail@gmail.com>
2016-07-11 16:47:51 +0200

    new constructor added

--- src/Model/PhysicalSeat.java
+++ src/Model/PhysicalSeat.java
@@ -1,7 +1,13 @@
 package Model;
 
 public class PhysicalSeat {
-
+    public PhysicalSeat(){
+    }
+    public PhysicalSeat(int seatNo, Type type){
+        this.seatNo = seatNo;
+        this.type = type;
+               
+    }
     private int seatNo;
 
     private Enum type;
