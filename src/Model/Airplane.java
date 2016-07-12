df2025ff4f33f1637c865e93d277da4f662d6e98
Niclas Wahlgren <niclaswmail@gmail.com>
2016-07-12 10:25:02 +0200

    Addad several methods

--- src/Model/Airplane.java
+++ src/Model/Airplane.java
@@ -4,22 +4,35 @@ import java.util.List;
 
 public class Airplane {
 
-    public Airplane(){
+    private List<PhysicalSeat> physicalSeats;
+
+    private String status;
+
+    public Airplane() {
     }
 
     public Airplane(List<PhysicalSeat> physicalSeats, String status) {
-        this.physicalSeat = physicalSeats;
+        this.physicalSeats = physicalSeats;
         this.status = status;
     }
     
-    private List<PhysicalSeat> physicalSeat;
+    public Airplane(int noOf1stClassSeats, int noOf2ndClassSeats, String status) {
+        for (int i = 0; i < noOf1stClassSeats; i++) {
+            PhysicalSeat ps = new Seat(i + 1, Type.FIRST_CLASS);
+            physicalSeats.add(ps);
+        }
+        for (int i = noOf1stClassSeats + 1; i < noOf1stClassSeats + noOf2ndClassSeats; i++) {
+            PhysicalSeat ps = new Seat(i + 1, Type.SECOND_CLASS);
+            physicalSeats.add(ps);
+        }
+        this.status = status;
+    }
 
-    private String status;
-    
-    List<PhysicalSeat> getPhysicalSeats(){
+    List<PhysicalSeat> getPhysicalSeats() {
         return physicalSeats;
     }
-    int getNumberOfPhysicalSeats(){
+
+    int getNumberOfPhysicalSeats() {
         return physicalSeats.size();
     }
 }
