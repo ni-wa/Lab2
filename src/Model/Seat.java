0f4e5f44e74cedfcfadd44da67c3631bb97c8236
Niclas Wahlgren <niclaswmail@gmail.com>
2016-07-12 08:51:09 +0200

    Added getters and setters for Seat

--- src/Model/Seat.java
+++ src/Model/Seat.java
@@ -7,6 +7,38 @@ public class Seat extends PhysicalSeat {
     private Food food;
 
     public boolean isBooked() {
-        throw new UnsupportedOperationException("Not supported yet*********");
+        if (getCustomer() == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @return the customer
+     */
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    /**
+     * @param customer the customer to set
+     */
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    /**
+     * @return the food
+     */
+    public Food getFood() {
+        return food;
+    }
+
+    /**
+     * @param food the food to set
+     */
+    public void setFood(Food food) {
+        this.food = food;
     }
 }
