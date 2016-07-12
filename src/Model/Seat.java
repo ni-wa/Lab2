4b11e96c94d4c6bdc35eadf91825a523525794ab
larslarslindholm <lindholm.edu@gmail.com>
2016-07-12 12:54:56 +0200

    removed comments

--- src/Model/Seat.java
+++ src/Model/Seat.java
@@ -3,7 +3,6 @@ package Model;
 public class Seat extends PhysicalSeat {
 
     private Customer customer;
-
     private Food food;
 
     public Seat(int seatNo, Type type){
@@ -19,30 +18,18 @@ public class Seat extends PhysicalSeat {
         }
     }
 
-    /**
-     * @return the customer
-     */
     public Customer getCustomer() {
         return customer;
     }
 
-    /**
-     * @param customer the customer to set
-     */
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
 
-    /**
-     * @return the food
-     */
     public Food getFood() {
         return food;
     }
 
-    /**
-     * @param food the food to set
-     */
     public void setFood(Food food) {
         this.food = food;
     }
